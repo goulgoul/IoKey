39a18c0f2e0e45aef2b0f19dd887870caef4d01a
goulgoul <antoine.gln571@gmail.com>
2024-01-23 16:40:04 +0100

    ajout LoraNode avant développement avec une autre lib

---
+++ LoRa_node/src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include "C:\\Users\\utilisateur\\Desktop\\IDesktopI\\Docs\\Cours\\5A GPSE - MSc IoT\\Demonstrateur\\LoRa_node\\.pio\\libdeps\\heltec_wifi_lora_32_V3\\Heltec ESP32 Dev-Boards\\src\\lora\\LoRa.h"
+
+void setup() {
+
+}
+
+void loop() {
+
+}
+
