8019305f8139e51fc630bbe1cee9e7eed6043614
Antoine Goulin <antoine.gln571@gmail.com>
2024-01-31 00:30:53 +0100

    quick push

--- IoKey_program/src/.h
+++
@@ -1,9 +0,0 @@
-#ifndef Global_Configuration_h
-#define Global_Configuration_h
-
-
-#define LORA_FRAME_LENGTH 8
-
-
-
-#endif // Global_Configuration_h
--- IoKey_program/src/main.cpp
+++ IoKey_program/src/main.cpp
@@ -48,32 +48,32 @@ void loop()
         case ESP_SLEEP_WAKEUP_TIMER:
             state = INITIALIZATION;
             break;
-
         default:
+            state = INITIALIZATION;
             break;
         }
         break;
+
     case INITIALIZATION:
         Serial.begin(BAUD_RATE);
 
         Wire.begin();
-        delay(1000);
         Wire.setClock(I2C_CLOCK_SPEED);
-        delay(1000);
-
-        lora->begin(lorawan_dev_eui, lorawan_app_eui, lorawan_app_key);
-        lora->print_parameters();
 
         top_sensor.begin();
         middle_sensor.begin();
         bottom_sensor.begin();
 
+        lora->begin(lorawan_dev_eui, lorawan_app_eui, lorawan_app_key);
+
+        // lora->print_parameters();
+
         state = MEASUREMENT;
         break;
 
     case MEASUREMENT:
-        data_to_send[0] = 0x01;
-        data_to_send[1] = 49;
+        data_to_send[0] = 1;    //? Device ID with respect to the use case
+        data_to_send[1] = 49;   //? Battery percentage
         data_to_send[2] = top_sensor.get_temperature();
         data_to_send[3] = middle_sensor.get_temperature();
         data_to_send[4] = bottom_sensor.get_temperature();
@@ -84,13 +84,6 @@ void loop()
         break;
 
     case MESSAGE_SENDING:
-        for (int i = 0; i < LORAWAN_FRAME_LENGTH; i++)
-        {
-            Serial.print(data_to_send[i]);
-            Serial.print(" ");
-        }
-        Serial.println();
-
         lora->join();
 
         lora->send_data((const uint8_t *)data_to_send, LORAWAN_FRAME_LENGTH);
@@ -101,6 +94,12 @@ void loop()
     case DEEP_SLEEP:
         esp_sleep_enable_ext0_wakeup(GPIO_NUM_7, 1);
         go_to_sleep();
+        break;
+
+    case BLE:
+
+
+        state = DEEP_SLEEP;
         break;
 
     default:
